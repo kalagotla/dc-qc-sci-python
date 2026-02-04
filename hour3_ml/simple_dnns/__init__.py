dc3eaa892d0977b5412eb47d410fcdf0d53440ac
kalagotla <dilip.kalagotla@gmail.com>
2026-02-04 09:42:48 -0500

    Updated hour 3. restructured the notebook and removed pretraining

--- hour3_ml/simple_dnns/__init__.py
+++ hour3_ml/simple_dnns/__init__.py
@@ -11,12 +11,6 @@ Available Classes:
     - NoiseData: Real-world airfoil noise data (requires data file)
     - Net: Configurable feedforward neural network
 
-Pre-trained Models:
-    Pre-trained model checkpoints are available in the pretrained_models subpackage:
-    
-    from simple_dnns.pretrained_models import MODELS_DIR, UNIVARIATE_MODEL
-    model.continue_train(filename=str(UNIVARIATE_MODEL))
-
 Example Usage:
     from simple_dnns import Univariate
     
@@ -31,7 +25,6 @@ from .bivariate import Bivariate
 from .simple_data import SimpleData
 from .noise_data import NoiseData
 from .network import Net
-from . import pretrained_models
 
 __version__ = "1.0.2"
 __author__ = "Dilip Kalagotla"
